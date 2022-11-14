d703949ac8f0bbec116277edc2b68be9a9428115
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-11-14 15:50:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['628823832771'],
+  ['628823832771'],
+  ['628823832771', 'MS', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6287855066799'
+global.author = 'мυʀѕι∂ вσт-χмℓ'
 
 global.multiplier = 69 // The higher, The harder levelup
 
