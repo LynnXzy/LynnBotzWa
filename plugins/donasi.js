3212d414ff1a76647584df38239834caf88fccb2
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-12-16 16:25:29 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,8 @@
 let handler =  m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+╭─「 Donasi • Reload 」
+│ • Maxis [60147675746]
+│ • Digi [60149431385]
+│ • Umobile [60189830350]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
